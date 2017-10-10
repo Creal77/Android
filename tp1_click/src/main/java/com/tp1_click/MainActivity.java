97ec9e19f94ca463afc4f846233d320ef549fb0d
Enzo Marioni <enzomarioni17@gmail.com>
2017-10-10 11:51:32 +0200

    Not End of TP1

--- tp1_click/src/main/java/com/tp1_click/MainActivity.java
+++ tp1_click/src/main/java/com/tp1_click/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         if(savedInstanceState != null) {
-
+            //TODO : Faire de truc
         }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);

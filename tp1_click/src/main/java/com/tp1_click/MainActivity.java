f318280576272c2fc126f85989859b43c339b835
Enzo Marioni <enzomarioni17@gmail.com>
2017-10-10 10:23:37 +0200

    Update Front

--- tp1_click/src/main/java/com/tp1_click/MainActivity.java
+++ tp1_click/src/main/java/com/tp1_click/MainActivity.java
@@ -2,12 +2,42 @@ package com.tp1_click;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] textouille= {
+      "Trololol",
+      "MDR",
+      "PTDMDR",
+      "ROFLMAO"
+    };
+    Integer num = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button)findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CheckBox checkBox = (CheckBox)findViewById(R.id.checkBox);
+                if(num == textouille.length) {
+                    num = 0;
+                }
+                TextView textView = (TextView)findViewById(R.id.textView);
+                if(checkBox.isChecked()){
+                    textView.setText(textouille[num].toUpperCase());
+                }
+                else {
+                    textView.setText(textouille[num].toLowerCase());
+                }
+                num++;
+            }
+        });
     }
 }
